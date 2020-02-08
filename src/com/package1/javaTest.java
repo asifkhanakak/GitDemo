76f3de5132605bdd63edc7557d23cd9931c8b474
asifkhanakak <56898816+asifkhanakak@users.noreply.github.com>
2020-02-07 19:58:44 -0500

    Update javaTest.java
    modified in gui

--- src/com/package1/javaTest.java
+++ src/com/package1/javaTest.java
@@ -4,7 +4,7 @@ public class javaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-           System.out.println("java test class");
+           System.out.println("java test Modified");
 	}
 
 }
